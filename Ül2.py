84e766e28f0f999c73ff392653873a11205df456
alex.karu <alex.karu@vikk.ee>
2023-01-20 11:34:31 +0200

    k

--- Ül2.py
+++ Ül2.py
@@ -4,18 +4,24 @@ import pygame  # impordib pygami
 pygame.init()
 screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption('2')
-font = pygame.font.Font(pygame.font.match_font('Times New Roman'), 50)
 
 bg = pygame.image.load('bg_shop.jpg')
-screen.blit(bg,[0,0])
+screen.blit(bg, [0, 0])
 
 seller = pygame.image.load('seller.png')
 seller = pygame.transform.scale(seller, [265, 308])
-screen.blit(seller,[100, 155])
+screen.blit(seller, [100, 155])
 
 chat = pygame.image.load('chat.png')
-chat = pygame.transform.scale(chat, [150, 60])
-screen.blit(chat,[0,0])
+chat = pygame.transform.scale(chat, [250, 200])
+screen.blit(chat, [250, 70])
+
+font = pygame.font.Font(pygame.font.match_font('Times New Roman'), 20)
+text = font.render("Tere, olen Alex Karu", True, [255, 255, 255])
+text_width = text.get_rect().width
+text_height = text.get_rect().height
+screen.blit(text, [300-text_width/10, 150-text_height/5])
+
 
 pygame.display.flip()
 # jätab avatud akna ekraanile
