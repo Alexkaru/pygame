da33ee510f88a79035db5a47f6ff2f3c6b684de1
alex.karu <alex.karu@vikk.ee>
2023-01-26 14:00:32 +0200

    k

--- ÜL3_ver.2.py
+++ ÜL3_ver.2.py
@@ -13,26 +13,16 @@ PINK = [255, 51, 204]
 PURPLE = [204, 0, 255]
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
-screen.fill(RED)
 
 
 class Square:
-    def __init__(self, color, sizea, sizeb):
-        self.color = color
+
+    def __int__(self, squarecolor, linecolor, sizea, sizeb):
+        self.squarecolor = squarecolor
+        self.linecolor = linecolor
         self.sizea = sizea
-        self.sizeb = sizeb
-
-    def make_square(self):
-        y = 1
-        for i in range(35):
-            x = 1
-            for j in range(38):
-                pygame.draw.rect(screen, self.color, [x, y, self.sizea, self.sizeb])
-                x += 18
-            y += 18
-
-
-Square.make_square(Square(YELLOW, 15, 15))
+        self.siseb = sizeb
+        
 
 pygame.display.flip()
 # jätab avatud akna ekraanile
