f0c5c7196417804326a5bac07c208ddc873b5fb4
alex.karu <alex.karu@vikk.ee>
2023-01-27 13:56:30 +0200

    k

--- Ül3.py
+++ Ül3.py
@@ -5,24 +5,24 @@ screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption("Harjutamine")
 
 
-GREEN = [153, 255, 153]
-RED = [255, 0, 0]
-BLUE = [0, 102, 255]
-YELLOW = [255, 255, 0]
-PINK = [255, 51, 204]
-PURPLE = [204, 0, 255]
-BLACK = [0, 0, 0]
-WHITE = [255, 255, 255]
-screen.fill(RED)
+GREEN = [153, 255, 153]  # Annab roherlise rgb väärtused
+RED = [255, 0, 0]  # Annab punase rgb väärtused
+BLUE = [0, 102, 255]  # Annab sinise rgb väärtused
+YELLOW = [255, 255, 0]  # Annab kollase rgb väärtused
+PINK = [255, 51, 204]  # Annab roosa rgb väärtused
+PURPLE = [204, 0, 255]  # Annab lillga rgb väärtused
+BLACK = [0, 0, 0]  # Annab musta rgb väärtused
+WHITE = [255, 255, 255]  # Annab valge rgb väärtused
+screen.fill(RED)  # Täidab ekraani punasega
 
 
-class Square:
-    def __init__(self, color, sizea, sizeb):
+class Square:  # Loob Square classi
+    def __init__(self, color, sizea, sizeb):  # Loob klassist objekti
         self.color = color
         self.sizea = sizea
         self.sizeb = sizeb
 
-    def make_square(self):
+    def make_square(self):  # Funktsioon ruutude tegemiseks
         y = 1
         for i in range(35):
             x = 1
@@ -32,7 +32,7 @@ class Square:
             y += 18
 
 
-Square.make_square(Square(YELLOW, 15, 15))
+Square.make_square(Square(GREEN, 15, 15))  # Teeb ruudud
 
 pygame.display.flip()
 # jätab avatud akna ekraanile
