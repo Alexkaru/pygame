95241926438df57af3aad22a2e83c1669f7a72e6
alex.karu <alex.karu@vikk.ee>
2023-02-07 10:35:44 +0200

    k

---
+++ For tsükkel ja järjend/ül 5 for tsükkel ja järjend.py
@@ -0,0 +1,15 @@
+# Alex Karu
+arvud = []
+
+
+def int_list(arv):
+    summ = 0
+    for n in arv:
+        summ += n
+        print(summ)
+
+
+if arvud == []:
+    print(0)
+print(int_list(arvud))
+
