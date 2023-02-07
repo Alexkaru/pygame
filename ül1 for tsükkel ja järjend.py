3ee64faee9dbd48fddff23f735b939d8076204bc
alex.karu <alex.karu@vikk.ee>
2023-02-07 09:45:55 +0200

    k

---
+++ ül1 for tsükkel ja järjend.py
@@ -0,0 +1,8 @@
+# Alex Karu
+list1 = ['Tartu', 'Tallin', 'Viljandi', 'Narva', 'Haapsalu', 'Paide', 'Valga']
+lisiti_pikkus = (len(list1))
+list1.sort()
+for linnad in list1:
+    print('Järjendis on: ', lisiti_pikkus, ' linna')
+    print(linnad)
+
---
+++ ül2 for tsükkel ja järjend.py
@@ -0,0 +1,7 @@
+# Alex Karu
+a = [2, 3, 1, 5]
+b = [6, 4]
+a.extend(b)
+a.sort()
+print(a)
+
