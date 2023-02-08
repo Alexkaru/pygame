299efff2ba7a58ca248350ecc865c00400fcfe32
alex.karu <alex.karu@vikk.ee>
2023-02-08 09:29:59 +0200

    k

--- For tsükkel ja järjend/ül 5 for tsükkel ja järjend.py
+++ For tsükkel ja järjend/ül 5 for tsükkel ja järjend.py
@@ -4,9 +4,9 @@ arvud = [10, 61, 2, 14, 42, 24, 7, 5]  # list
 
 def arvude_jarjend(arv):  # funktsioon, mis lisab järjendis aru prindib selle välja nii kaua, kui järjendis on arve
     summa = 0
-    for n in arv:
-        summa += n
-        print(summa)
+    for n in arv:  # tsükkel
+        summa += n  # inkremendib summa n võrra
+        print(summa)  # prindib summa
 
 
 if arvud == []:  # kui arvud on tühi list prindib null
