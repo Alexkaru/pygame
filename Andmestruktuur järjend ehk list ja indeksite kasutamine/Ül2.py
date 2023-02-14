0bf63c98bef0f5ed1c68d52f7b1df0b7ed9a5ce1
alex.karu <alex.karu@vikk.ee>
2023-02-14 09:46:42 +0200

    k

---
+++ Andmestruktuur järjend ehk list ja indeksite kasutamine/Ül2.py
@@ -0,0 +1,30 @@
+# Alex Karu
+
+a = [1, 12, -3, 4, 0]
+
+#  ÜL1
+#  a.remove(1)
+#  print(a)
+
+#  ÜL2
+#  a.remove(1)
+#  a.remove(0)
+#  print(a)
+
+#  ÜL3
+#  a.sort()
+#  print(a[0])
+
+#  ÜL4
+#  a.sort()
+#  print(a)
+
+#  ÜL5
+#  a.append(9)
+#  print(a)
+
+#  ÜL6
+#  a.pop(0)
+#  a.insert(0, 5)
+#  print(a)
+
