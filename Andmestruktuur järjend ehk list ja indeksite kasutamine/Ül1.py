fd9c4f60160a1c27f08dad15d2664e90cfc806e2
alex.karu <alex.karu@vikk.ee>
2023-02-14 09:36:54 +0200

    k

---
+++ Andmestruktuur järjend ehk list ja indeksite kasutamine/Ül1.py
@@ -0,0 +1,18 @@
+# Alex Karu
+tunniplaan = ["mate", "inka", "vene", "kunst", "arvuti"]
+
+#  ÜL1
+print(tunniplaan)
+
+#  ÜL2
+print(tunniplaan[0])
+
+#  ÜL3
+print(len(tunniplaan))
+
+#  ÜL4
+if "kunst" in tunniplaan:
+    print('Kunst on tunniplaanis')
+
+else:
+    print('Kunst ei ole tunniplaanis')
