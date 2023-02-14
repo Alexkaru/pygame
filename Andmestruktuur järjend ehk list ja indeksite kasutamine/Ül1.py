4c3a002d9571b45d97aaaaabd133e3e419768167
alex.karu <alex.karu@vikk.ee>
2023-02-14 10:28:50 +0200

    k

--- Andmestruktuur järjend ehk list ja indeksite kasutamine/Ül1.py
+++ Andmestruktuur järjend ehk list ja indeksite kasutamine/Ül1.py
@@ -2,17 +2,17 @@
 tunniplaan = ["mate", "inka", "vene", "kunst", "arvuti"]
 
 #  ÜL1
-print(tunniplaan)
+#  print(tunniplaan)
 
 #  ÜL2
-print(tunniplaan[0])
+#  print(tunniplaan[0])
 
 #  ÜL3
-print(len(tunniplaan))
+#  print(len(tunniplaan))
 
 #  ÜL4
-if "kunst" in tunniplaan:
-    print('Kunst on tunniplaanis')
+#  if "kunst" in tunniplaan:
+#      print('Kunst on tunniplaanis')
 
-else:
-    print('Kunst ei ole tunniplaanis')
+#  else:
+#      print('Kunst ei ole tunniplaanis')
