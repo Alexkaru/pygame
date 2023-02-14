8b91b92a8dc1ac4a6365349e05f2ae6663722c59
alex.karu <alex.karu@vikk.ee>
2023-02-14 09:15:20 +0200

    k

--- pygame/Ül2.py
+++ pygame/Ül2.py
@@ -7,21 +7,21 @@ screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption('ÜL2')
 
 # Blokk lisab ekraanile tagatausta milleks on pood
-bg = pygame.image.load('bg_shop.jpg')  # Lisab tagatusta pildi
+bg = pygame.image.load('Ül2 pildid/bg_shop.jpg')  # Lisab tagatusta pildi
 screen.blit(bg, [0, 0])  # Muudab pildi asukohta ekraanil
 
 # Blokk lisab müüa, muudab selle asukohta ja suurust
-seller = pygame.image.load('seller.png')
+seller = pygame.image.load('Ül2 pildid/seller.png')
 seller = pygame.transform.scale(seller, [265, 308])  # Muudab müüa suurust
 screen.blit(seller, [100, 155])
 
 # Blokk lisab tekstimulli ja muudab selle suurust ja sukohta ekraanil
-chat = pygame.image.load('chat.png')
+chat = pygame.image.load('Ül2 pildid/chat.png')
 chat = pygame.transform.scale(chat, [250, 200])
 screen.blit(chat, [250, 70])
 
 # blokk lisab vikk logo ekraani paremasse nurka, muudab selle suurust ja ka asukohta ekraanil
-vikklogo = pygame.image.load('vikklogo.png')
+vikklogo = pygame.image.load('Ül2 pildid/vikklogo.png')
 vikklogo = pygame.transform.scale(vikklogo, [320, 45])
 screen.blit(vikklogo, [10, 10])
 
@@ -33,12 +33,12 @@ pygame.draw.line(screen, [255, 255, 255], [240, 10], [240, 55], 1)  # [värv], [
 pygame.draw.arc(screen, [255, 255, 255], [228, 8, 50, 50], -3.14/3, 1)  # [värv] [rect_kordinaadid], [nurk], joonepaksus
 
 # Blokk Lisab koogi ja muudab selle suurust ja asukohta ekraanil
-cake = pygame.image.load('cake.png')
+cake = pygame.image.load('Ül2 pildid/cake.png')
 cake = pygame.transform.scale(cake, [100, 100])
 screen.blit(cake, [400, 180])
 
 # Blokk Lisab mõõga ja muudab selle suurust ja asukohta ekraanil
-mook = pygame.image.load('mook.png')
+mook = pygame.image.load('Ül2 pildid/mook.png')
 mook = pygame.transform.scale(mook, [50, 50])
 screen.blit(mook, [20, 200])
 
