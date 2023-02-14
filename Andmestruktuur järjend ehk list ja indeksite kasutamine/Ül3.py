e83ecbf9707dc21769d25a09f88ab02aae0d4604
alex.karu <alex.karu@vikk.ee>
2023-02-14 10:03:36 +0200

    k

---
+++ Andmestruktuur järjend ehk list ja indeksite kasutamine/Ül3.py
@@ -0,0 +1,24 @@
+# Alex Karu
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+
+#  ÜL1
+#  a.extend(b)
+#  print(a)
+
+#  ÜL2
+#  b.insert(len(a)+1, 4)
+#  print(b)
+
+#  ÜL3
+#  c = sum(b)
+#  print(c)
+
+#  ÜL4
+g = 0
+while True:
+    print(a[g])
+    g += 1
+
+    if len(a) == g:
+        break
