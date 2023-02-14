1738cf53fc9b25d97b48923c2734140564286f43
alex.karu <alex.karu@vikk.ee>
2023-02-14 10:25:23 +0200

    k

---
+++ Andmestruktuur järjend ehk list ja indeksite kasutamine/Ül4.py
@@ -0,0 +1,25 @@
+# Alex Karu
+
+#  ÜL1
+#  a = []
+#  for i in range(3):
+#      a.append(i)
+#  print(a)
+
+#  ÜL2
+#  for i in range(2, 5):
+#      print(i)
+
+#  ÜL3
+#  for i in range(2, 7, 2):
+#      print(i)
+
+#  ÜL4
+#  for i in range(-1, -4, -2):
+#      print(i)
+
+#  ÜL5
+#  korrutis = 1
+#  for i in range(1,6,2):
+#      korrutis *= i
+#  print(korrutis)
