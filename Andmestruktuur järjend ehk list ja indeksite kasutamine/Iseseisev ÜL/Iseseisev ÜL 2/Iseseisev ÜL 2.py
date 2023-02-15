39701b983b25fee1d110039c4ef56a4e8331a107
alex.karu <alex.karu@vikk.ee>
2023-02-15 13:11:31 +0200

    k

--- Andmestruktuur järjend ehk list ja indeksite kasutamine/Iseseisev ÜL/Iseseisev ÜL 2/Iseseisev ÜL 2.py
+++ Andmestruktuur järjend ehk list ja indeksite kasutamine/Iseseisev ÜL/Iseseisev ÜL 2/Iseseisev ÜL 2.py
@@ -1,10 +1,39 @@
 #  Alex Karu
+import turtle
 
 
+def juhendi_lugemine(kilpkonn):
+    juhendid = []
+    with open(kilpkonn, 'r') as f:
+        for rida in f:
+            cmd = rida.strip().split(' ')
+            juhendid.append(cmd)
+    return juhendid
 
-jarjend = []
-f = open("kilpkonn.txt")
-for rida in f:
-    jarjend.append(rida.strip())
-f.close()
-print(jarjend)
+
+def joonistamine(kujund, kilpkonn):
+    for cmd in kujund:
+        if cmd[0] == 'edasi':
+            kilpkonn.forward(int(cmd[1]))
+        elif cmd[0] == 'tagasi':
+            kilpkonn.backward(int(cmd[1]))
+        elif cmd[0] == 'paremale':
+            kilpkonn.right(int(cmd[1]))
+        elif cmd[0] == 'vasakule':
+            kilpkonn.left(int(cmd[1]))
+
+
+kordade_arv = int(input("Mitu korda kujund joonistada? "))
+juhend = juhendi_lugemine('kilpkonn.txt')
+a = input("Sisestage programmi pealkiri ")
+b = input("Sisestage värv (inglise keeles) ")
+turtle.bgcolor(b)
+turtle.title(a)
+aken = turtle.Screen()
+k = turtle.Turtle()
+k.speed(1)
+
+
+for i in range(kordade_arv):
+    joonistamine(juhend, k)
+turtle.exitonclick()
