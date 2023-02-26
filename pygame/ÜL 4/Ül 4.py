31207a6f5ab595cc4566d8fa57c183b3702d52ae
Alexkaru <115535254+Alexkaru@users.noreply.github.com>
2023-02-26 20:02:27 +0200

    Update Ül 4.py

--- pygame/ÜL 4/Ül 4.py
+++ pygame/ÜL 4/Ül 4.py
@@ -1,71 +1,116 @@
 # Alex Karu
 import pygame
+import random
 import sys
 
-pygame.init()
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode([640, 480])
+pygame.init()  # alustame pygame'i
+screen = pygame.display.set_mode((640, 480))  # loome ekraani
+pygame.display.set_caption("ÜÖ 4 Alex Karu")  # lisame pealkirja
+clock = pygame.time.Clock()  # Loome 'clock' objekti
+skoor = 0  # loome skoori
 
-punaneposx = 290
+bg = pygame.image.load("bg.jpg")  # laadime taustapildi
+bg2 = pygame.image.load("bg.jpg")  # laadime teise taustapildi
+bgposX = 0  # esimese taustapildi X positsioon
+bg2posX = 480  # teise taustapildi X positsioon
+BGkiirus = -3  # tausta liikmis kiirus
 
-sinine_auto = pygame.image.load("f1_sinine.jpg")
-sinine_auto = pygame.transform.scale(sinine_auto, [60, 150])
-sinine_posx, sinine_posy = 170, -210
+sinine_auto = pygame.image.load("f1_sinine.jpg")  # laadime sinise auto
+sinine_auto2 = pygame.image.load("f1_sinine.jpg")  # laadime teise sinise auto pildi
+punane_auto = pygame.image.load("f1_punane.jpg")  # laadime punase auto pildi
+gameover = False  # seme gamover muutuja väärtuse vääraks
+Kiirus = 3  # sinise auto kiirus
 
-sinine_auto2 = pygame.image.load("f1_sinine.jpg")
-sinine_auto2 = pygame.transform.scale(sinine_auto2, [60, 150])
-sinine_posx2, sinine_posy2 = 420, -300
+Sinine_posy = random.randint(0, 100)  # sinise auto positsioon Y
+Sinine_posy2 = random.randint(0, 100)  # teise sinise auto positsioon Y
+Punane_posx, Punane_posy = 298, 390  # punase auto positsioonid
+Punane_kiirus = 0  # punase auto kiirus
+Sinine_posx = random.randint(310, 450)  # sinise auto positsioon X
+Sinine_posx2 = random.randint(140, 270)  # teise sinise auto positsioon X
 
-kiirus = 3
-score_font = pygame.font.SysFont("times New Roman", 30)
+while not gameover:  # kordub, kuni gameover muutuja on väär
+    clock.tick(100)  # seadistame fps-i
 
+    for event in pygame.event.get():  # for loop
+        if event.type == pygame.QUIT:  # kui aken suletakse
+            sys.exit()  # lõpetame mängu
 
-def skoor(score):
-    value = score_font.render("Sinu skoor: " + str(score), True, [0, 0, 0])
-    screen.blit(value, [0, 0])
+    # taustapiltide lisamine
+    screen.blit(bg, (0, bgposX))
+    screen.blit(bg2, (0, bg2posX))
 
+    # kiiruse kasutamine
+    bgposX -= BGkiirus
+    bg2posX -= BGkiirus
 
-lopp_skoor = 0
-mang_labi = False
-while not mang_labi:
-    clock.tick(60)
-    # mängu sulgemine ristist
-    events = pygame.event.get()
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            sys.exit()
+    # kui taustapilt jõuab lõppu, viime tagasi algusesse
+    if bgposX >= 480:
+        bgposX = -480
+    if bg2posX >= 480:
+        bg2posX = -480
 
-    if sinine_posy >= 480:
-        sinine_posy = -210
-        lopp_skoor += 1
+    # siniste autode lisamine
+    screen.blit(sinine_auto, (Sinine_posx, Sinine_posy))
+    screen.blit(sinine_auto2, (Sinine_posx2, Sinine_posy2))
 
-    if sinine_posy2 >= 480:
-        sinine_posy2 = -300
-        lopp_skoor += 1
+    # autode liigutamine
+    Sinine_posy += Kiirus + 0.8
+    Sinine_posy2 += Kiirus + 1
 
+    # punase auto lisamine
+    screen.blit(punane_auto, (Punane_posx, Punane_posy))
+    Punane_posy += Punane_kiirus  # auto liigutamine
 
-    screen.blit(sinine_auto, (sinine_posx, sinine_posy))
-    sinine_posy += kiirus
-    pygame.display.flip()
+    # skoori näitamine
+    screen.blit(pygame.font.Font(None, 30).render(f"Skoor: {skoor}", True, [255, 255, 255]), [10, 20])
 
-    screen.blit(sinine_auto2, (sinine_posx2, sinine_posy2))
-    sinine_posy2 += kiirus
-    pygame.display.flip()
+    # autode positsiooni taastamine
+    if Sinine_posy >= 480:  # kui auto jõuab lõppu
+        Sinine_posy = -120  # taastame auto positsiooni
+        skoor += 1  # suurendame skoori
+        Sinine_posx = random.randint(130, 280)  # paneme auto suvalisse kohta
 
-    background = pygame.image.load("bg.jpg")
-    background = pygame.transform.scale(background, [640, 480])
-    screen.blit(background, [0, 0])
+    if Sinine_posy2 >= 480:  # kui teine auto jõuab lõppu
+        Sinine_posy2 = -120  # taastame positsiooni
+        skoor += 1  # suurendame skoori
+        Sinine_posx2 = random.randint(300, 480)  # paneme auto suvalisse kohta
 
-    punane_auto = pygame.image.load("f1_punana.jpg")
-    punane_auto = pygame.transform.scale(punane_auto, [60, 150])
-    screen.blit(punane_auto, [punaneposx, 310])
+    if Punane_posx >= 640:  # kui punane auto lahkub ekraanilt
+        Punane_posx = 520  # liigutame selle tagasi
 
+    if Punane_posx <= 0:  # kui punane auto lahkub ekraanilt
+        Punane_posx = 80  # liigutame selle tagasi
+
+    # punase auto liigutamine
     key = pygame.key.get_pressed()  # saame vajutatud klahvi
     if key[pygame.K_LEFT]:  # kui vasak klahv
-        punaneposx -= 5  # liigutame autot vasakule
+        Punane_posx -= 5  # liigutame autot vasakule
     if key[pygame.K_RIGHT]:  # kui parem klahv
-        punaneposx += 5  # liigutame autot paremale
+        Punane_posx += 5  # liigutame autot paremale
+    if key[pygame.K_a]:  # kui a klahv
+        Punane_posx -= 5  # liigutame autot vasakule
+    if key[pygame.K_d]:  # kui d klahv
+        Punane_posx += 5  # liigutame autot paremale
 
-    skoor(lopp_skoor)
+    # mängu lõpp, kui sinine auto puudutab punast
+    if Punane_posy + 55 >= Sinine_posy >= Punane_posy - 55:  # kui sinine ja punane auto on samal kohal
+        if Punane_posx + 50 >= Sinine_posx >= Punane_posx - 50:  # kui sinine ja punane auto on ka samal kohal
+            gameover = True  # lõpetame mängu
 
-pygame.quit()
+    if Punane_posy + 55 >= Sinine_posy2 >= Punane_posy - 55:  # kui teine sinine auto ja punane auto on samal kohal
+        if Punane_posx + 50 >= Sinine_posx2 >= Punane_posx - 50:  # kui teine sinine auto ja punane auto on ka samal kohal
+            gameover = True  # lõpetame mängu
+
+    pygame.display.flip()  # värskendame ekraani
+
+while True:  # kontroll, kas mäng on läbi
+    if gameover:  # kui mäng on läbi
+        # kuvame "Mäng läbi!"
+        screen.blit(pygame.font.Font(None, 50).render("Mäng läbi!", True, [255, 255, 255]), [230, 300])
+        # kuvame skoori
+        screen.blit(pygame.font.Font(None, 50).render(f"Skoor: {skoor}", True, [255, 255, 255]), [240, 200])
+        pygame.display.flip()  # uuendame ekraani
+
+    for event in pygame.event.get():  # for loop sündmuse saamiseks
+        if event.type == pygame.QUIT:  # kui ekraan suletakse
+            sys.exit()  # lõpetame mängu
