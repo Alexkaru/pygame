a6dcdd3dcf3e54ba873b34ad659e3c58b7c77de3
alex.karu <alex.karu@vikk.ee>
2023-03-06 12:45:57 +0200

    k

--- pygame/ÜL 5/ÜL5.py
+++ pygame/ÜL 5/ÜL5.py
@@ -1,76 +1,62 @@
 # Impordime vajalikud moodulid
 import pygame
 import sys
-pygame.init()  # alustame pygame mooduli
+pygame.init()  # alustame pygame'i
 
+screenX = 640  # ekraani laius
+screenY = 480  # ekraani pikkus
+screen = pygame.display.set_mode([screenX, screenY])  # ekraani loomine
+pygame.display.set_caption("ÜL5 - Alex Karu")  # ekraanile pealkirja lisamine
+screen.fill([173, 216, 230])  # täidab ekraani hele sinisega
+clock = pygame.time.Clock()  # clock objekti loomine
 
-# Seadistame ekraani seaded
-screenX = 640
-screenY = 480
-screen = pygame.display.set_mode([screenX, screenY])
-pygame.display.set_caption("ÜL4 - Alex Karu")
-screen.fill([173, 216, 230])
-clock = pygame.time.Clock()
+ball_x = 0  # balli x kordinaat
+ball_y = 0  # balli y kordinaat
+kiirus_x = 3  # balli x kiirus
+kiirus_y = 4  # balli y kiirus
+pad_x = 0  # aluse x kordinaat
+pad_y = screenY/1.5  # aluse y kordinaat
+kiirus_pad = 4  # aluse kiirus
 
-# Seadistame palli kiiruse ja positsiooni
-ball_x = 0
-ball_y = 0
-kiirus_x = 3
-kiirus_y = 4
+ball_rect = pygame.Rect(ball_x, ball_y, 20, 20)  # palli kordinaadi ristküllik
+ball_img = pygame.image.load("ball.png")  # palli pildi laadimine
+ball_img = pygame.transform.scale(ball_img, (20, 20))  # palli pildi suuruse muutmine
+pad_rect = pygame.Rect(pad_x, pad_y, 120, 20)  # aluse kordinaadi ristküllik
+pad_img = pygame.image.load("pad.png")  # aluse pildi laadimine
+pad_img = pygame.transform.scale(pad_img, (120, 20))  # aluse pildi suuruse muutmine
+skoor = 0  # skoori seadistamine
 
-# Seadistame aluse kiiruse ja positsiooni
-pad_x = 0
-pad_y = screenY/1.5
-kiirus_pad = 4
-
-# Piltide laadimine
-ball_rect = pygame.Rect(ball_x, ball_y, 20, 20)
-ball_img = pygame.image.load("ball.png")
-ball_img = pygame.transform.scale(ball_img, (20, 20))
-pad_rect = pygame.Rect(pad_x, pad_y, 120, 20)
-pad_img = pygame.image.load("pad.png")
-pad_img = pygame.transform.scale(pad_img, (120, 20))
-
-# Scoori muutuja seadistamine
-skoor = 0
-
-gameover = False  # gameover muutuja seadistamine
-while not gameover:  # kordub, kuni gameover muutuja on False
-    clock.tick(60)  # seadistame kaadrisageduse
-    for event in pygame.event.get():  # sündmuse käitlemine
-        if event.type == pygame.QUIT:  # kui aken suletakse
+gameover = False  # gameover muutuja vääraks muutmine
+while not gameover:  # kordub kuni gameover muutuja on väär
+    clock.tick(60)  # fps'i seadistamine
+    for event in pygame.event.get():  # for loop sündmuse saamiseks
+        if event.type == pygame.QUIT:  # kui sündmus on akna sulemine
             sys.exit()  # lõpetame mängu
 
-    # Palli liikumine
-    pall = pygame.Rect(ball_x, ball_y, 20, 20)
-    screen.blit(ball_img, pall)
-    ball_x += kiirus_x
-    ball_y += kiirus_y
+    pall = pygame.Rect(ball_x, ball_y, 20, 20)  # palli asukoha seadistamine
+    screen.blit(ball_img, pall)  # palli asukoha muutmine
+    ball_x += kiirus_x  # palli x teljel liikumine
+    ball_y += kiirus_y  # palli y telje liikumine
 
-    # Aluse liikumine
-    pad_rect = pygame.Rect(pad_x, pad_y, 120, 20)
-    screen.blit(pad_img, pad_rect)
-    pad_x += kiirus_pad
+    pad_rect = pygame.Rect(pad_x, pad_y, 120, 20)  # aluse asukoha seadistamine
+    screen.blit(pad_img, pad_rect)  # aluse asukoha muutmine
+    pad_x += kiirus_pad  # aluse liigutamine
 
-    # Skoori kuvamine
-    screen.blit(pygame.font.Font(None, 30).render(f"Skoor: {skoor}", True, [255, 255, 255]), [10, 20])
+    screen.blit(pygame.font.Font(None, 30).render(f"Skoor: {skoor}", True, [255, 255, 255]), [10, 20])  # skoori kuvamine
 
-    # Kui puudutab ekraani ääri, muudab palli suunda
-    if ball_x > screenX - ball_img.get_rect().width or ball_x < 0:
-        kiirus_x = -kiirus_x
-    if ball_y > screenY-ball_img.get_rect().height or ball_y < 0:
-        kiirus_y = -kiirus_y
+    if ball_x > screenX - ball_img.get_rect().width or ball_x < 0:  # kui pall puudutab ekraani vasakut või paremat äärt
+        kiirus_x = -kiirus_x  # muudab see oma suunda
+    if ball_y > screenY-ball_img.get_rect().height or ball_y < 0:  # kui pall puudutab ekraani ülemist või alumist äärt
+        kiirus_y = -kiirus_y  # muudab see oma suunda
 
-    # Kui puudutab alust, muudab palli suunda ja suurendab skoori
-    if pall.colliderect(pad_rect) and kiirus_y > 0:
-        kiirus_y = -kiirus_y
-        skoor += 1
+    if pall.colliderect(pad_rect) and kiirus_y > 0:  # kui palli kordinaadi ristküllik puutub aluse kordinaadi ristküllikut
+        kiirus_y = -kiirus_y  # muudab see suunda
+        skoor += 1  # ja lisab skoorile 1
 
-    # Kui puudutab ekraani ääri, muudab aluse suunda
-    if pad_x > screenX - pad_img.get_rect().width or pad_x < 0:
-        kiirus_pad = -kiirus_pad
+    if pad_x > screenX - pad_img.get_rect().width or pad_x < 0:  # kui alus puudutab ekraani äärt
+        kiirus_pad = -kiirus_pad  # muudab see oma suunda
 
     pygame.display.flip()  # ekraani uuendamine
     screen.fill([173, 216, 230])  # ekraani täitmine hele sinisega
 
-pygame.quit()
+pygame.quit()  # pygame'i sulgemine
